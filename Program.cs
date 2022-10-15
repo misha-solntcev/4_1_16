3326c4bf94ff82156682c34dbf10ac94501b5dce
misha-solntcev <misha.solntcev@gmail.com>
2022-10-15 16:36:44 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Вариант 16
+В заданной целочисленной матрице A(N, M) выведите на печать индексы первого
+положительного элемента, кратного заданному числу К. Если таких элементов в матрице
+нет, то выведите соответствующий текст. */
+
+namespace _4_1_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[,]
+            {   
+                { 7, 0, 6, -4, 9 },
+                { 3, 1, 16, -7, 8 },
+                { 7, 8, 9, 0, 2 },
+                { 12, 0, 3, -4, 5 },
+                { 13, 8, 9, 0, 4 },
+            };
+            int n = arr.GetLength(0);
+            int m = arr.GetLength(1);
+
+            int k = 4;
+            bool flag = false;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if ((arr[i, j] > 0) & (arr[i, j] % k == 0))
+                    {
+                        Console.WriteLine($"Первое число arr[{i}, {j}] = {arr[i, j]}");
+                        flag = true;
+                        goto Exit;
+                    }                        
+                }
+            }
+            Exit:
+            if (!flag)
+                Console.WriteLine($"Такого число нет.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("4_1_16")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4_1_16")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4a1831a1-1e85-4ec6-a58f-0737bb14b41e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
